49abdabfdcd11332a26a96ad2d98fedb4e700cbb
Robert MacDavid <robert@opennetworking.org>
2021-09-28 16:24:58 -0400

    some comments, gitignore

--- python/heavy_hitters.py
+++ python/heavy_hitters.py
@@ -2,6 +2,7 @@ import random
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from typing import Tuple, Dict, List, Callable, Optional
+
 from hashing import make_crc16_func, CRC16_DEFAULT_POLY
 
 
@@ -114,12 +115,25 @@ class CountMinSketch(HeavyHitterSketch):
         self.arrays = [[0] * self.height for _ in range(self.width)]
         self.ground_truth = defaultdict(int)
 
-    def set(self, key: Tuple[int], insert_val: int = 1):
+    def set(self, key: Tuple[int], insert_val: int = 1) -> None:
+        """
+        Set the CMS value for the given item key. Overwrites every array cell that `key` hashes to.
+        Useful for treating this class like a bloom filter, albeit an inefficient one.
+        :param key: item key
+        :param insert_val: value to set for the given item key.
+        :return: None
+        """
         self.ground_truth[key] = insert_val
         for array, index in zip(self.arrays, self.indices(key)):
             array[index] = insert_val
 
     def add(self, key: Tuple[int], add_val: int = 1) -> int:
+        """
+        Add `add_val` to the CMS for the given item key
+        :param key: item key
+        :param add_val: value to add to the item's value
+        :return: The CMS value for the given item key, after the addition
+        """
         self.ground_truth[key] += add_val
         smallest = None
         for array, index in zip(self.arrays, self.indices(key)):
@@ -129,6 +143,13 @@ class CountMinSketch(HeavyHitterSketch):
         return smallest
 
     def add_after_return(self, key: Tuple[int], add_val: int = 1) -> int:
+        """
+        Add `add_val` to the CMS for the given item key. Same as `add()`, but returns the CMS value pre-addition
+        instead of post-addition.
+        :param key: item key
+        :param add_val: value to add to the item's value
+        :return: The CMS value for the given item key, before the addition occurred
+        """
         # Same as add, but returns the old value before the addition occurred
         self.ground_truth[key] += add_val
         smallest = None
@@ -139,6 +160,11 @@ class CountMinSketch(HeavyHitterSketch):
         return smallest
 
     def get(self, key: Tuple[int]) -> int:
+        """
+        Get the CMS value for the given item key
+        :param key: item key
+        :return: CMS value ie the min of all values the item key hashes to
+        """
         return min(array[index] for array, index in zip(self.arrays, self.indices(key)))
 
     def clear(self):
@@ -146,12 +172,28 @@ class CountMinSketch(HeavyHitterSketch):
         self.ground_truth.clear()
 
     def subtract(self, key: Tuple[int], sub_val: int) -> int:
+        """
+        Subtract `sub_val` from the CMS for the given item key
+        :param key: item key
+        :param sub_val: value to subtract from the item's value
+        :return: The resulting CMS value after subtraction
+        """
         return self.add(key, add_val=-sub_val)
 
     def get_all(self, key: Tuple[int]) -> Tuple[int]:
+        """
+        Return one value from each array for the given item key, instead of just the min.
+        :param key: item key
+        :return: all values that the item key hashed to
+        """
         return tuple(array[index] for array, index in zip(self.arrays, self.indices(key)))
 
     def indices(self, key: Tuple[int]) -> Tuple[int]:
+        """
+        Return indices (hash values modulo array lengths) for all arrays in the CMS for the given key.
+        :param key: the key to hash
+        :return: a tuple of array indices
+        """
         return tuple(hash_func(*key, salt) % self.height
                      for hash_func, salt in zip(self.hash_funcs, self.salts))
 
