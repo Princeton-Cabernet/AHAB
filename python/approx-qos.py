8a043f7ab365261c7dd07ce7ce89a89ea6701418
Robert MacDavid <robert@opennetworking.org>
2021-09-29 23:57:44 -0400

    some work on qos struct, trimmed obsolete code

--- python/approx-qos.py
+++ python/approx-qos.py
@@ -3,19 +3,16 @@ import math
 import random
 from collections import defaultdict
 
-from typing import List, Callable, Tuple, Optional, Dict
+from typing import List, Callable, Tuple, Optional, Dict, Set
 import numpy as np
 from heavy_hitters import HeavyHitterSketch, ExactHeavyHitters, CountMinSketch
 from hashing import make_crc16_func
-from estimators import ThresholdHistograms, CapacityHistograms
+from estimators import ThresholdHistograms, ThresholdNewtonMethod, CapacityHistograms, correct_threshold
 
 import matplotlib.pyplot as plt
 
 SEED = 0x123456
 random.seed(SEED)  # for reproducible tests
-
-
-
 
 
 class ApproxQos:
@@ -29,7 +26,7 @@ class ApproxQos:
     threshold_estimators: List[ThresholdHistograms]
     capacity_estimator: CapacityHistograms
 
-    drops: Dict[Tuple[int], int]
+    dropped_bytes_per_flow: Dict[Tuple[int], int]
 
     def __init__(self, slice_weights: List[float], base_station_capacity: int,
                  hh_instance: Optional[HeavyHitterSketch]):
@@ -44,23 +41,28 @@ class ApproxQos:
             # which are currently per-base-station.
             self.flow_size_sketch = hh_instance
 
-        self.threshold_estimators = [ThresholdHistograms() for _ in range(self.num_slices)]
+        self.threshold_estimators = [ThresholdNewtonMethod() for _ in range(self.num_slices)]
         self.capacity_estimator = CapacityHistograms(self.slice_weights, base_station_capacity)
 
         heaviest_weight = max(self.slice_weights)
+        # there is no rounding error here if every weight is a power of 2, as will be the case in tofino
         self.scale_factors = [int(heaviest_weight / weight) for weight in self.slice_weights]
 
-        self.drops = defaultdict(int)
+        self.dropped_bytes_per_flow = defaultdict(int)
 
-    def process_packet(self, packet_size: int, slice_id: int, packet_key: Tuple[int]):
-        scaled_size = packet_size * self.scale_factors[slice_id]
-        flow_size = self.flow_size_sketch.add_after_return(packet_key, scaled_size)
+    def process_packet(self, packet_size: int, slice_id: int, packet_key: Tuple[int]) -> bool:
+        scale_factor = self.scale_factors[slice_id]
+        scaled_size = packet_size * scale_factor
+        estimated_flow_size = int(self.flow_size_sketch.add_after_return(packet_key, scaled_size) / scale_factor)
         threshold = self.threshold_estimators[slice_id].get_current_threshold()
-        if flow_size > threshold:
-            self.drops[packet_key] += 1
+        drop: bool = estimated_flow_size > threshold
+        if drop:
+            self.dropped_bytes_per_flow[packet_key] += packet_size
 
-        self.threshold_estimators[slice_id].process_packet(scaled_size, flow_size)
+        self.threshold_estimators[slice_id].process_packet(packet_size, estimated_flow_size)
         self.capacity_estimator.process_packet(slice_id=slice_id, pkt_size=packet_size)
+
+        return drop
 
     def end_epoch(self):
         self.capacity_estimator.end_epoch()
@@ -69,204 +71,84 @@ class ApproxQos:
         self.flow_size_sketch.clear()
 
 
+class ApproxQosWithSavedStats(ApproxQos):
+    relative_flow_size_drop_rate_pairs: List[Tuple[float, float]]
+    dropped_bytes_per_flow_this_epoch: Dict[Tuple[int], int]
+    received_bytes_per_flow_this_epoch: Dict[Tuple[int], int]
+    slice_to_flows: Dict[int, Set[Tuple[int]]]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.relative_flow_size_drop_rate_pairs = []
+        self.clear_per_epoch_structs()
+
+    def clear_per_epoch_structs(self):
+        self.dropped_bytes_per_flow_this_epoch = defaultdict(int)
+        self.received_bytes_per_flow_this_epoch = defaultdict(int)
+        self.slice_to_flows = defaultdict(set)
+
+    def process_packet(self, packet_size: int, slice_id: int, packet_key: Tuple[int]) -> bool:
+        drop = super().process_packet(packet_size, slice_id, packet_key)
+
+        self.received_bytes_per_flow_this_epoch[packet_key] += packet_size
+        if drop:
+            self.dropped_bytes_per_flow_this_epoch[packet_key] += packet_size
+        self.slice_to_flows[slice_id].add(packet_key)
+
+        return drop
+
+    def end_epoch(self):
+        super().end_epoch()
+        flow_sizes = self.received_bytes_per_flow_this_epoch
+        for slice_id in range(self.num_slices):
+            capacity = self.capacity_estimator.capacity_for(slice_id)
+            flow_ids = self.slice_to_flows[slice_id]
+            true_threshold = correct_threshold([flow_sizes[flow_id] for flow_id in flow_ids], capacity)
+            for flow_id in flow_ids:
+                flow_size = flow_sizes[flow_id]
+                dropped_bytes = self.dropped_bytes_per_flow_this_epoch[flow_id]
+                relative_flow_size = flow_size / true_threshold
+                drop_rate = dropped_bytes / flow_size
+
+                self.relative_flow_size_drop_rate_pairs.append((relative_flow_size, drop_rate))
+
+        self.clear_per_epoch_structs()
 
 
-"""
-have X light streams and Y heavy streams
-in each epoch, light streams send A packets and heavy streams send Y packets. Order them randomly. Create
-arrays with duplicates to count as packets [X,X,X,X,Y,Y,Y,Y]. Shuffle them, then iterate and place them in a hash table
-if the bucket is full, increment "drop count" 
-"""
-MAX_INT = (1 << 32) - 1
+class PerfectQos:
+    num_slices: int
+    slice_weights: List[float]
+    total_capacity: int
 
+    bytes_per_flow: Dict[Tuple[int], int]
+    slice_to_flows: Dict[int, Set[Tuple[int]]]
 
-def get_distinct_random_ints(count: int, min_val: int = 0, max_val: int = MAX_INT):
-    if count > (max_val - min_val):
-        return None
-    if min_val > max_val:
-        return None
-    numbers = set([random.randint(min_val, max_val) for _ in range(count)])
-    while len(numbers) < count:
-        numbers.add(random.randint(min_val, max_val))
-    return list(numbers)
+    capacity_estimator: CapacityHistograms
 
+    dropped_bytes_per_flow: Dict[Tuple[int], int]
 
-def experiment():
-    """
-    Experiment using zipf distribution across flow sizes
-    :return:
-    """
-    zipf_exponent = 1.2
-    epochs = 10
-    link_capacity = 200000
-    packets_per_epoch = int(link_capacity * 1.2)
+    def __init__(self, slice_weights: List[float], base_station_capacity: int):
+        self.total_capacity = base_station_capacity
+        self.clear_per_window_structs()
+        self.capacity_estimator = CapacityHistograms(self.slice_weights, base_station_capacity)
 
-    slice_weights = [2**(-1), 2**(-2), 2**(-3), 2**(-3)]
-    num_slices = len(slice_weights)
+    def clear_per_window_structs(self):
+        self.bytes_per_flow = defaultdict(int)
+        self.slice_to_flows = defaultdict(set)
 
-    drops = defaultdict(int)
-    flow_sizes = defaultdict(int)
+    def process_packet(self, packet_key: Tuple[int], packet_size: int, slice_id: int):
+        self.bytes_per_flow[packet_key] += packet_size
+        self.slice_to_flows[slice_id].add(packet_key)
+        self.capacity_estimator.process_packet(slice_id=slice_id, pkt_size=packet_size)
 
-    rng = np.random.default_rng(SEED)
-
-    for epoch in range(epochs):
-        print("Epoch:", epoch)
-
-        packets = rng.zipf(a=zipf_exponent, size=packets_per_epoch)
-
-        for packet in packets:
-            flow_sizes[packet] += 1
-            epoch_salt = (epoch & 0xffff) << (_crc16(packet) % 16)
-            if cms.add(packet, pepper=epoch_salt) > bucket_threshold:
-                drops[packet] += 1
-
-    print("Flows:", len(flow_sizes))
-
-    drop_rates = [(flow_size / epochs, drops[packet]/flow_size) for packet, flow_size in flow_sizes.items()]
-    drop_rates.sort(key=lambda tup: tup[0])
-
-    x = [a for (a, b) in drop_rates]
-    y = [b for (a, b) in drop_rates]
-
-    num_light_flows = 0
-    light_flows_with_bad_drops = 0
-
-    for (flow_size, drop_rate) in drop_rates:
-        if flow_size < bucket_threshold:
-            num_light_flows += 1
-            if drop_rate > 0.01:
-                light_flows_with_bad_drops += 1
-    print("%d light flows, %d have bad drop rates"
-          % (num_light_flows, light_flows_with_bad_drops))
-
-
-    fig, ax = plt.subplots()
-
-    ax.set_title("%d flows, %d epochs, %d per-epoch bucket capacity"
-                 % (len(flow_sizes), epochs, bucket_threshold))
-    ax.set_ylabel("Drop rate")
-    ax.set_xlabel("Flow size (packets per epoch)")
-    ax.set_xscale("log")
-
-    ax.scatter(x, y, alpha=0.5)
-    plt.show()
-
-
-def experiment():
-    clever = True
-    COLUMNS = 2
-    ROWS = 65536 // COLUMNS
-    BUCKET_THRESHOLD = 100
-    LIGHT_STREAMS = 500000
-    LIGHT_STREAM_RATE = 10
-    HEAVY_STREAMS = 10
-    HEAVY_STREAM_RATE = 10000
-
-    ITERATIONS = 10
-
-    lightStreamDrops = 0
-    heavyStreamDrops = 0
-
-    cms: Optional[CountMinSketch] = None
-    buckets: Optional[List[int]] = None
-
-    if clever:
-        print("Using count-min sketch")
-        hash_funcs = [make_crc16_func(polynomial=CRC16_DEFAULT_POLY + (0x100 * i)) for i in range(COLUMNS)]
-        salts = [random.randint(0, MAX_INT) for _ in range(COLUMNS)]
-        cms = CountMinSketch(hash_funcs=hash_funcs, salts=salts, height=ROWS)
-    else:
-        print("Using simple buckets")
+    def end_epoch(self):
+        self.capacity_estimator.end_epoch()
+        for slice_id in range(self.num_slices):
+            slice_capacity = self.capacity_estimator.capacity_for(slice_id)
+            threshold = correct_threshold(list(self.bytes_per_flow.values()), self.total_capacity)
+            for flow_id in self.slice_to_flows[slice_id]:
+                self.dropped_bytes_per_flow[flow_id] += max(self.bytes_per_flow[flow_id] - threshold, 0)
 
 
 
-    print("Generating random flow IDs")
-    flowIds = get_distinct_random_ints(LIGHT_STREAMS + HEAVY_STREAMS)
-    print("Done")
-
-    light_flow_ids = set(flowIds[:LIGHT_STREAMS])
-    heavy_flow_ids = set(flowIds[LIGHT_STREAMS + 1:])
-
-    drops = defaultdict(int)
-
-    for iteration in range(ITERATIONS):
-        print("Iteration %d" % iteration)
-        # clear the buckets for a new epoch
-        if clever:
-            cms.clear()
-        else:
-            buckets = [0] * COLUMNS
-
-        # generate "packets"
-        packets = []
-        for key in light_flow_ids:
-            packets.extend([key] * LIGHT_STREAM_RATE)
-        for key in heavy_flow_ids:
-            packets.extend([key] * HEAVY_STREAM_RATE)
-        random.shuffle(packets)
-
-        # iterate through
-        for flowId in packets:
-            drop = False
-            epoch_salt = (iteration & 0xffff) << (_crc16(flowId) % 16)
-            if clever:
-                if cms.add(flowId, epoch_salt) > BUCKET_THRESHOLD:
-                    drop = True
-            else:
-                index = _crc16(flowId, epoch_salt)
-                buckets[index] += 1
-                if buckets[index] > BUCKET_THRESHOLD:
-                    drop = True
-            if drop:
-                drops[flowId] += 1
-
-    print("Done iterating")
-
-    lightDropRates = []
-    heavyDropRates = []
-    for key in light_flow_ids:
-        dropRate = drops[key] / (LIGHT_STREAM_RATE * ITERATIONS)
-        lightDropRates.append(dropRate)
-        lightDropRates.sort()
-    for key in heavy_flow_ids:
-        dropRate = drops[key] / (HEAVY_STREAM_RATE * ITERATIONS)
-        heavyDropRates.append(dropRate)
-        heavyDropRates.sort()
-
-    def get_percent_slots(array: List, percents: List[float]):
-        return tuple([array[min(math.floor(len(array) * percent), len(array) - 1)] for percent in percents])
-
-    nonZeroLightDropRates = []
-    for i in range(len(lightDropRates)):
-        if lightDropRates[i] > 0.0:
-            nonZeroLightDropRates = lightDropRates[i:]
-            break
-
-    print("%d light flows of %d had non-zero drops" % (len(nonZeroLightDropRates), LIGHT_STREAMS))
-    print("Light drop rates: ...", lightDropRates[-20:])
-    print("Heavy drop rates:", heavyDropRates)
-    print("Light drop rates: 90%%: %f, 95%%: %f, 99%%: %f, 99.9%%: %f, 99.99%%: %f, 100%%: %f"
-          % get_percent_slots(lightDropRates, [0.9, 0.95, 0.99, 0.999, 0.9999, 1.0]))
-
-
-    fig, ax = plt.subplots()
-
-    ax.set_title("%d light flows, %d heavy flows, %d epochs, %d bucket threshold"
-                 % (LIGHT_STREAMS, HEAVY_STREAMS, ITERATIONS, BUCKET_THRESHOLD))
-    ax.set_ylabel("Drop rate")
-    ax.set_xlabel("Light flow index")
-
-    # Using set_dashes() to modify dashing of an existing line
-    line1, = ax.plot([_ for _ in range(len(lightDropRates))], lightDropRates, label='Light flow drop rates')
-    line1.set_dashes([2, 2, 10, 2])  # 2pt line, 2pt break, 10pt line, 2pt break
-
-    # Using plot(..., dashes=...) to set the dashing when creating a line
-    # line2, = ax.plot(x, y - 0.2, dashes=[6, 2], label='Using the dashes parameter')
-
-    ax.legend()
-    plt.show()
-
-
-
-if __name__ == "__main__":
-    experiment2()
