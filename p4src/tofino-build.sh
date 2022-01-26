db21b9a6778e880fd4d5673e0315371c26d3f05b
vagrant <vagrant@vagrant.vm>
2022-01-26 04:48:45 +0000

    better build script

--- p4src/tofino-build.sh
+++ p4src/tofino-build.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-./gen_include_files.py
+if ./gen_include_files.py; then
+	echo "Generated include files"
+else
+	echo "Include file generation failed."
+	exit 1
+fi
 
-bf-p4c --Wdisable=uninitialized_use afd.p4
+
+if bf-p4c --Wdisable=uninitialized_use afd.p4; then
+	echo "Compilation succeeded"
+else
+	echo "Compilation failed"
+	exit 1
+fi
 
 if [ -n "$SDE_INSTALL" ]; then
 	echo "Installing compiler output in $SDE_INSTALL"
-	mv afd.tofino/afd.conf $SDE_INSTALL/share/p4/targets/tofino/afd.conf
-	rm -rf $SDE_INSTALL/afd.tofino
-	mv afd.tofino $SDE_INSTALL/
+	sudo mv afd.tofino/afd.conf $SDE_INSTALL/share/p4/targets/tofino/afd.conf
+	sudo rm -rf $SDE_INSTALL/afd.tofino
+	sudo mv afd.tofino $SDE_INSTALL/
 else
 	echo "Var SDE_INSTALL not set. Not installing compiler output"
 fi
